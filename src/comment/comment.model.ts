c9782d28b884da9c39ccd74a82a9a232807eba8a
Almog Vagman Ciprut <almogvc@gmail.com>
2018-09-12 14:23:21 +0300

    refactor: Add comments model

--- src/comment/comment.model.ts
+++ src/comment/comment.model.ts
@@ -1,10 +1,32 @@
 
 import * as mongoose from 'mongoose';
 import { IComment } from './comment.interface';
+import { commentValidatorConfig } from './validator/comment.validator.config';
 
 const commentSchema: mongoose.Schema = new mongoose.Schema(
     {
-        property: { type: String, required: true },
+        text: {
+            type: String,
+            required: true,
+            validate: {
+                validator: (text: string) => {
+                    return ((text.length >= commentValidatorConfig.text.minLength) &&
+                        (text.length <= commentValidatorConfig.text.maxLength));
+                },
+            },
+        },
+        video: {
+            type: String,
+            required: true,
+        },
+        parent: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comments',
+        },
+        user: {
+            type: String,
+            required: true,
+        },
     },
     {
         autoIndex: false,
