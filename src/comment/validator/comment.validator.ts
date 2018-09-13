17b610c3a3974263b60156f7cf419480851c897d
Almog Vagman Ciprut <almogvc@gmail.com>
2018-09-13 15:33:16 +0300

    refactor: Add route validations

--- src/comment/validator/comment.validator.ts
+++ src/comment/validator/comment.validator.ts
@@ -1,30 +1,33 @@
 import { Request, Response, NextFunction } from 'express';
 import { CommentValidatons } from './comment.validations';
-import { PropertyInvalidError, IdInvalidError } from '../../utils/errors/userErrors';
+import {
+    TextTooShortError,
+    CommentIdNotValidError,
+    TextTooLongError,
+    UserIdNotValidError,
+    VideoIdNotValidError,
+} from '../../utils/errors/userErrors';
 import { IComment } from '../comment.interface';
 
 export class CommentValidator {
 
     static canCreate(req: Request, res: Response, next: NextFunction) {
-        next(CommentValidator.validateProperty(req.body.comment.property));
-    }
-
-    static canCreateMany(req: Request, res: Response, next: NextFunction) {
-        const propertiesValidations: (Error | undefined)[] = req.body.comments.map((comment: IComment) => {
-            return CommentValidator.validateProperty(comment.property);
-        });
-
-        next(CommentValidator.getNextValueFromArray(propertiesValidations));
+        next(
+            CommentValidator.validateVideo(req.body.comment.video) ||
+            CommentValidator.validateParent(req.body.comment.parent) ||
+            CommentValidator.validateText(req.body.comment.text) ||
+            CommentValidator.validateUser(req.body.comment.user),
+        );
     }
 
     static canUpdateById(req: Request, res: Response, next: NextFunction) {
         next(
             CommentValidator.validateId(req.params.id) ||
-            CommentValidator.validateProperty(req.body.comment.property));
-    }
-
-    static canUpdateMany(req: Request, res: Response, next: NextFunction) {
-        next(CommentValidator.validateProperty(req.body.comment.property));
+            CommentValidator.validateVideo(req.body.comment.video) ||
+            CommentValidator.validateParent(req.body.comment.parent) ||
+            CommentValidator.validateText(req.body.comment.text) ||
+            CommentValidator.validateUser(req.body.comment.user),
+        );
     }
 
     static canDeleteById(req: Request, res: Response, next: NextFunction) {
@@ -36,20 +39,39 @@ export class CommentValidator {
     }
 
     static canGetOne(req: Request, res: Response, next: NextFunction) {
-        next();
+        next(
+            CommentValidator.validateVideo(req.query.video) ||
+            CommentValidator.validateParent(req.query.parent) ||
+            CommentValidator.validateText(req.query.text) ||
+            CommentValidator.validateUser(req.query.user),
+        );
     }
 
     static canGetMany(req: Request, res: Response, next: NextFunction) {
-        next();
+        next(
+            CommentValidator.validateVideo(req.query.video) ||
+            CommentValidator.validateParent(req.query.parent) ||
+            CommentValidator.validateText(req.query.text) ||
+            CommentValidator.validateUser(req.query.user),
+        );
     }
 
     static canGetAmount(req: Request, res: Response, next: NextFunction) {
-        next();
+        next(
+            CommentValidator.validateVideo(req.query.video) ||
+            CommentValidator.validateParent(req.query.parent) ||
+            CommentValidator.validateText(req.query.text) ||
+            CommentValidator.validateUser(req.query.user),
+        );
     }
 
-    private static validateProperty(property: string) {
-        if (!CommentValidatons.isPropertyValid(property)) {
-            return new PropertyInvalidError();
+    private static validateText(text: string) {
+        if (!CommentValidatons.isTextLengthTooShort(text)) {
+            return new TextTooShortError();
+        }
+
+        if (!CommentValidatons.isTextLengthTooLong(text)) {
+            return new TextTooLongError();
         }
 
         return undefined;
@@ -57,7 +79,31 @@ export class CommentValidator {
 
     private static validateId(id: string) {
         if (!CommentValidatons.isIdValid(id)) {
-            return new IdInvalidError();
+            return new CommentIdNotValidError();
+        }
+
+        return undefined;
+    }
+
+    private static validateUser(user: string) {
+        if (!CommentValidatons.isUserValid(user)) {
+            return new UserIdNotValidError();
+        }
+
+        return undefined;
+    }
+
+    private static validateVideo(video: string) {
+        if (!CommentValidatons.isVideoValid(video)) {
+            return new VideoIdNotValidError();
+        }
+
+        return undefined;
+    }
+
+    private static validateParent(parent: string) {
+        if (!CommentValidatons.isParentValid(parent)) {
+            return new CommentIdNotValidError();
         }
 
         return undefined;
