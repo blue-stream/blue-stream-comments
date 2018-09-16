08b995c6f761724fc29c158e51b6ecf1da217984
Almog Vagman Ciprut <almogvc@gmail.com>
2018-09-16 13:43:40 +0300

    refactor: Change object type to IComment

--- src/comment/validator/comment.mocks.ts
+++ src/comment/validator/comment.mocks.ts
@@ -2,24 +2,31 @@ import { Types } from 'mongoose';
 import { createRequest, createResponse } from 'node-mocks-http';
 import { sign } from 'jsonwebtoken';
 import { config } from '../../config';
+import { IComment } from '../comment.interface';
 
 export const responseMock = createResponse();
 
 export class ValidRequestMocks {
-    readonly validProperty: string = '12345';
-    readonly validProperty2: string = '23456';
-    readonly validProperty3: string = '34567';
 
-    readonly comment = {
-        property: this.validProperty,
+    readonly comment: IComment = {
+        video: (new Types.ObjectId()).toHexString(),
+        parent: (new Types.ObjectId()).toHexString(),
+        text: 'comment text',
+        user: 'a@a',
     };
 
-    readonly comment2 = {
-        property: this.validProperty2,
+    readonly comment2: IComment = {
+        video: (new Types.ObjectId()).toHexString(),
+        parent: (new Types.ObjectId()).toHexString(),
+        text: 'comment text 2',
+        user: 'a@b',
     };
 
-    readonly comment3 = {
-        property: this.validProperty3,
+    readonly comment3: IComment = {
+        video: (new Types.ObjectId()).toHexString(),
+        parent: (new Types.ObjectId()).toHexString(),
+        text: 'comment text 3',
+        user: 'b@b',
     };
 
     readonly commentFilter = this.comment;
@@ -37,36 +44,9 @@ export class ValidRequestMocks {
         },
     });
 
-    createMany = createRequest({
-        method: 'POST',
-        url: '/api/comment/many/',
-        headers: {
-            authorization: this.authorizationHeader,
-        },
-        body: {
-            comments: [
-                this.comment,
-                this.comment2,
-                this.comment3,
-            ],
-        },
-    });
-
-    updateMany = createRequest({
-        method: 'PUT',
-        url: '/api/comment/many',
-        headers: {
-            authorization: this.authorizationHeader,
-        },
-        body: {
-            commentFilter: this.commentFilter,
-            comment: this.comment,
-        },
-    });
-
     updateById = createRequest({
         method: 'PUT',
-        url: '/api/comment/:id',
+        url: '/api/comment/',
         headers: {
             authorization: this.authorizationHeader,
         },
@@ -80,7 +60,7 @@ export class ValidRequestMocks {
 
     deleteById = createRequest({
         method: 'DELETE',
-        url: '/api/comment/:id',
+        url: '/api/comment/',
         headers: {
             authorization: this.authorizationHeader,
         },
@@ -91,7 +71,7 @@ export class ValidRequestMocks {
 
     getOne = createRequest({
         method: 'GET',
-        url: `/api/comment/one?commentFilter={'property':${this.validProperty}}`,
+        url: `/api/comment/one`,
         headers: {
             authorization: this.authorizationHeader,
         },
@@ -100,7 +80,7 @@ export class ValidRequestMocks {
 
     getMany = createRequest({
         method: 'GET',
-        url: `/api/comment/many?commentFilter={'property':${this.validProperty}}`,
+        url: `/api/comment/many`,
         headers: {
             authorization: this.authorizationHeader,
         },
@@ -109,7 +89,7 @@ export class ValidRequestMocks {
 
     getAmount = createRequest({
         method: 'GET',
-        url: `/api/comment/amount?commentFilter={'property':${this.validProperty}}`,
+        url: `/api/comment/amount`,
         headers: {
             authorization: this.authorizationHeader,
         },
@@ -118,7 +98,7 @@ export class ValidRequestMocks {
 
     getById = createRequest({
         method: 'GET',
-        url: '/api/comment/:id',
+        url: '/api/comment/',
         headers: {
             authorization: this.authorizationHeader,
         },
