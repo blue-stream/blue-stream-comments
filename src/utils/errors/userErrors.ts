a7eaca5ecef5f29bae242090c3d093bfa84a4ef3
Almog Vagman Ciprut <almogvc@gmail.com>
2018-11-18 17:02:17 +0200

    style: Change string type

--- src/utils/errors/userErrors.ts
+++ src/utils/errors/userErrors.ts
@@ -8,7 +8,7 @@ export class IdNotFoundError extends UserError {
 
 export class VideoIdNotValidError extends UserError {
     constructor(message?: string) {
-        super(message || `Video's ID is not valid`, 400);
+        super(message || 'Video\'s ID is not valid', 400);
     }
 }
 
@@ -20,30 +20,30 @@ export class CommentNotValidError extends UserError {
 
 export class CommentTextNotValidError extends CommentNotValidError {
     constructor(message?: string) {
-        super(message || `Comment's text is not valid`);
+        super(message || 'Comment\'s text is not valid');
     }
 }
 
 export class TextTooShortError extends CommentNotValidError {
     constructor(message?: string) {
-        super(message || `Comment's text is too short`);
+        super(message || 'Comment\'s text is too short');
     }
 }
 
 export class TextTooLongError extends CommentNotValidError {
     constructor(message?: string) {
-        super(message || `Comment's text is too long`);
+        super(message || 'Comment\'s text is too long');
     }
 }
 
 export class CommentIdNotValidError extends CommentNotValidError {
     constructor(message?: string) {
-        super(message || `Comment's ID is not valid`);
+        super(message || 'Comment\'s ID is not valid');
     }
 }
 
 export class UserIdNotValidError extends CommentNotValidError {
     constructor(message?: string) {
-        super(message || `User's ID is not valid`);
+        super(message || 'User\'s ID is not valid');
     }
 }
