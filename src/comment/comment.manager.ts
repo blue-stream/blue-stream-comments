84b3fc2ebd780c7b9545328ae58c29c4931f885b
Almog Vagman Ciprut <almogvc@gmail.com>
2018-12-17 14:16:33 +0200

    fix: Fix getRootComments by returning id instead of _id

--- src/comment/comment.manager.ts
+++ src/comment/comment.manager.ts
@@ -49,6 +49,7 @@ export class CommentManager implements CommentRepository {
     static async getRootComments(resourceId: string, startIndex: number, endIndex: number) {
         const comments: IComment[] = await CommentRepository.getMany({ resource: resourceId, parent: null }, startIndex, endIndex);
         const RepliesAmountPromise: Promise<number>[] = [];
+        console.log(comments);
 
         comments.forEach((comment: IComment) => {
             RepliesAmountPromise.push(CommentManager.getRepliesAmount(comment.id));
@@ -57,7 +58,7 @@ export class CommentManager implements CommentRepository {
         const RepliesAmount: number[] = await Promise.all(RepliesAmountPromise);
 
         return comments.map((comment: IComment, index: number) => {
-            return { ...(comment as any)._doc, repliesAmount: RepliesAmount[index] };
+            return { ...(comment as any).toJSON(), repliesAmount: RepliesAmount[index] };
         });
     }
 
