a2f4b049c588aab9ff8de2d92526e46421adc2ac
Almog Vagman Ciprut <almogvc@gmail.com>
2018-12-31 15:23:04 +0200

    refactor: Update rabbit

--- src/comment/comment.broker.publish.ts
+++ src/comment/comment.broker.publish.ts
@@ -3,6 +3,6 @@ import * as rabbit from '../utils/rabbit';
 export class CommentPublishBroker {
 
     public static async publish(routingKey: string, message: any) {
-        rabbit.publish('application', routingKey, message);
+        rabbit.publish('application', 'topic', routingKey, message);
     }
 }
